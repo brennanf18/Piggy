da528ba321cbfb500b3ba0fc568243f1c1e98719
brennanf18 <70531420+brennanf18@users.noreply.github.com>
2020-09-24 11:32:05 -0400

    Update student.py

--- student.py
+++ student.py
@@ -62,7 +62,7 @@ class Piggy(PiggyParent):
         
         #lower0-ordered example...
         
-        for x in range(5):
+        for x in range(1):
             pass
             self.forward_skipp()
             self.right_twirl()
