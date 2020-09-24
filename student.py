3ed1a089de30bc8962b5d0f3cd5ffc0b4b152abe
brennanf18 <70531420+brennanf18@users.noreply.github.com>
2020-09-24 11:28:54 -0400

    Update student.py

--- student.py
+++ student.py
@@ -80,9 +80,9 @@ class Piggy(PiggyParent):
 
     def right_twirl(self):
         for x in range(2)
-        self.right(primary = 50, counter = -50)
-        time.sleep(.5)
-        self.stop()
+            self.right(primary = 50, counter = -50)
+            time.sleep(.5)
+            self.stop()
 
 
     def safe_to_dance(self):
