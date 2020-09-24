b32931604c80103da7c9c8e311d8900a437fea24
brennanf18 <70531420+brennanf18@users.noreply.github.com>
2020-09-24 11:29:38 -0400

    Update student.py

--- student.py
+++ student.py
@@ -79,7 +79,7 @@ class Piggy(PiggyParent):
             self.stop()
 
     def right_twirl(self):
-        for x in range(2)
+        for x in range(2):
             self.right(primary = 50, counter = -50)
             time.sleep(.5)
             self.stop()
