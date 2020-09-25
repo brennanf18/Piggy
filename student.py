fd273b78509d63b2efbba3da0f98ddb076b0e0c4
brennanf18 <70531420+brennanf18@users.noreply.github.com>
2020-09-25 11:05:20 -0400

    Update student.py

--- student.py
+++ student.py
@@ -90,8 +90,8 @@ class Piggy(PiggyParent):
     
     def left_twirl(self):
         for x in range(4):
-            self.turn_by_deg(180)
-            self.turn_by_deg(180)
+            self.turn_by_deg(-180)
+            self.turn_by_deg(-180)
             self.stop()
 
     
