a3a0170fbca02dcc438e9c5676243dd9301a11ec
brennanf18 <70531420+brennanf18@users.noreply.github.com>
2020-09-29 10:57:56 -0400

    Update student.py

--- student.py
+++ student.py
@@ -144,7 +144,7 @@ class Piggy(PiggyParent):
         """ Does a 360 distance check and returns true if safe """
         # Check for all fail/early-termination conditions
         for _ in range(4):
-            if self.read_distance()< 300:
+            if self.read_distance()< 600:
                 print("NOT SAFE TO DANCE!!!")
                 return False
             else:
