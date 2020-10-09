7cb754ac850240557234ece332184e63d38e3d59
brennanf18 <70531420+brennanf18@users.noreply.github.com>
2020-10-09 10:49:27 -0400

    changing indent again

--- student.py
+++ student.py
@@ -199,9 +199,9 @@ class Piggy(PiggyParent):
     def scan(self):
         """Sweep the servo and populate the scan_data dictionary"""
         for angle in range(self.MIDPOINT-350, self.MIDPOINT+350, 10):
-            self.scan_data = OrderedDict(sorted(self.scan_data.items())): 
-                self.servo(angle)
-                self.scan_data[angle] = self.read_distance()
+            self.scan_data = OrderedDict(sorted(self.scan_data.items())) 
+            self.servo(angle)
+            self.scan_data[angle] = self.read_distance()
 
     def obstacle_count(self):
         """Does a 360 scan and returns the number of obstacles it sees"""
