cee50b5cff3bfeb8a52cb6adc7c324d3b2068d4c
brennanf18 <70531420+brennanf18@users.noreply.github.com>
2020-10-21 11:25:34 -0400

    changing my nav format

--- student.py
+++ student.py
@@ -321,24 +321,31 @@ class Piggy(PiggyParent):
         
         # TODO: build self.quick_check() that does a fast, 3-part check instead of read_distance
         
+        exit_ang = self.get_heading() # Andrew helped me with this code
         turn_count = 0
 
+        self.fwd()
         while True:
             if not self.quick_check():
                 turn_count += 1
-                self.back()
-                time.sleep(0.3)
                 self.stop()
-                #self.turn_until_clear()
-                if turn_count % 5 == 0:
-                    #self.turn_to_deg(exit_ang)
-                    self.turn_until_clear()
+                if turn_count % 6 == 0:
+                    self.back()
+                    time.sleep(.75)
+                    self.stop()
+                    self.turn_by_deg(exit_ang)
                 elif 'l' in self.right_or_left():
-                    self.turn_by_deg(45)
+                    self.turn_by_deg(-45)
                 else:
                     self.turn_by_deg(45)
+                    self.turn_until_clear()
+
             else:
                 self.fwd()
+            time.sleep(.1)
+            
+
+
 
 
     
