22bba82dc0d4619c2355ad6e4a8d5d963cb5cf2a
brennanf18 <70531420+brennanf18@users.noreply.github.com>
2020-10-21 11:05:47 -0400

    made scan faster

--- student.py
+++ student.py
@@ -228,7 +228,7 @@ class Piggy(PiggyParent):
     
     def scan(self):
         """Sweep the servo and populate the scan_data dictionary"""
-        for angle in range(self.MIDPOINT-450, self.MIDPOINT+451, 40):
+        for angle in range(self.MIDPOINT-450, self.MIDPOINT+451, 60):
             self.servo(angle)
             self.scan_data[angle] = self.read_distance()
         # sort data for easier analysis
